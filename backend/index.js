8ddca730ee9cfa55c491f7772ef409bc48408fa2
jôáo <jpsconceicao18@gmail.com>
2023-10-09 11:15:20 -0300

    CRUD pronto

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,18 @@
 const express = require("express");
-const res = require("express/lib/response");
+
 const app = express();
-const PORT = 666;
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
+const PORT = 791;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
 
 const mysql = require("mysql2/promise");
-const conection = mysql.createPool({
+const connection = mysql.createPool({
   host: "localhost",
   port: 3306,
   database: "EJRMoney",
@@ -19,287 +20,382 @@ const conection = mysql.createPool({
   password: "",
 });
 
-// seleciona todos os usuários do database
-const getAllPessoas = async () => {
-  const [query] = await conection.execute("select * from usuario");
+app.post("/salvar/usuario", async (req, res) => {
+  const { nome, email, senha } = req.body;
+  const [query] = await connection.execute(
+    "insert into usuario (nome, email, senha) values (?, ?, ?)",
+    [nome, email, senha]
+  );
+  return res.json(query);
+});
+
+app.post("/salvar/categoria/receita", async (req, res) => {
+  const { nome, tipo } = req.body;
+  const [query] = await connection.execute(
+    "insert into categoria_receita (nome, tipo) values (?, ?)",
+    [nome, tipo]
+  );
+  return res.json(query);
+});
+
+app.post("/salvar/categoria/despesa", async (req, res) => {
+  const { nome, tipo } = req.body;
+  const [query] = await connection.execute(
+    "insert into categoria_despesa (nome, tipo) values (?, ?)",
+    [nome, tipo]
+  );
+  return res.json(query);
+});
+
+app.post("/salvar/despesa", async (req, res) => {
+  const { descricao, valor, data, categoria_despesa_id } = req.body;
+  const [query] = await connection.execute(
+    "insert into despesas (descricao, valor, data, categoria_despesa_id) values (?, ?, ?, ?)",
+    [descricao, valor, data, categoria_despesa_id]
+  );
+  return res.json(query);
+});
+
+app.post("/salvar/receita", async (req, res) => {
+  const { descricao, valor, data, categoria_despesa_id } = req.body;
+  const [query] = await connection.execute(
+    "insert into receitas (descricao, valor, data, categoria_receita_id) values (?, ?, ?, ?)",
+    [descricao, valor, data, categoria_despesa_id]
+  );
+  return res.json(query);
+});
+
+const getAllUsuarios = async () => {
+  const [query] = await connection.execute("select * from usuario");
   return query;
 };
 
-// retorna a lista de usuários
-app.get("/usuario", async (req, res) => {
-  const resultado = await getAllPessoas();
+const getAllCategoriaReceitas = async () => {
+  const [query] = await connection.execute("select * from categoria_receita");
+  return query;
+};
 
-  if (resultado.length === 0) {
-    return res
-      .status(200)
-      .json({ mensagem: "Nenhum usuário encontrado no database!" });
-  }
+const getAllCategoriaDespesas = async () => {
+  const [query] = await connection.execute("select * from categoria_despesa");
+  return query;
+};
+
+const getAllReceitas = async () => {
+  const [query] = await connection.execute("select * from receitas");
+  return query;
+};
+
+const getAllDespesas = async () => {
+  const [query] = await connection.execute("select * from despesas");
+  return query;
+};
+
+app.get("/usuario", async (req, res) => {
+  const resultado = await getAllUsuarios();
   return res.status(200).json(resultado);
 });
 
-// retorna o usuário pelo seu id
+app.get("/categoria/receita", async (req, res) => {
+  const resultado = await getAllCategoriaReceitas();
+  return res.status(200).json(resultado);
+});
+
+app.get("/categoria/despesa", async (req, res) => {
+  const resultado = await getAllCategoriaDespesas();
+  return res.status(200).json(resultado);
+});
+
+app.get("/despesas", async (req, res) => {
+  const resultado = await getAllDespesas();
+  return res.status(200).json(resultado);
+});
+
+app.get("/receitas", async (req, res) => {
+  const resultado = await getAllReceitas();
+  return res.status(200).json(resultado);
+});
+
 app.get("/usuario/:id", async (req, res) => {
   const { id } = req.params;
-  const [query] = await conection.execute(
+  const [query] = await connection.execute(
     "select * from usuario where id = ?",
     [id]
   );
   if (query.length === 0)
-    return res.status(400).json({ mensagem: "Nenhum usuário encontrado!" });
+    return res.status(400).json({ mensagem: "Nenhum usuario encontrado" });
   return res.status(200).json(query);
 });
 
-// retorna o usuário pelo nome
+app.get("/categoria/receita/:id", async (req, res) => {
+  const { id } = req.params;
+  const [query] = await connection.execute(
+    "select * from categoria_receita where id = ?",
+    [id]
+  );
+  if (query.length === 0)
+    return res.status(400).json({ mensagem: "Nenhuma receita encontrada" });
+  return res.status(200).json(query);
+});
+
+app.get("/categoria/despesa/:id", async (req, res) => {
+  const { id } = req.params;
+  const [query] = await connection.execute(
+    "select * from categoria_despesa where id = ?",
+    [id]
+  );
+  if (query.length === 0)
+    return res.status(400).json({ mensagem: "Nenhuma receita encontrada" });
+  return res.status(200).json(query);
+});
+
+app.get("/receitas/:id", async (req, res) => {
+  const { id } = req.params;
+  const [query] = await connection.execute(
+    "select * from receitas where id = ?",
+    [id]
+  );
+  if (query.length === 0)
+    return res.status(400).json({ mensagem: "Nenhuma receita encontrada" });
+  return res.status(200).json(query);
+});
+
+app.get("/despesas/:id", async (req, res) => {
+  const { id } = req.params;
+  const [query] = await connection.execute(
+    "select * from despesas where id = ?",
+    [id]
+  );
+  if (query.length === 0)
+    return res.status(400).json({ mensagem: "Nenhuma despesa encontrada" });
+  return res.status(200).json(query);
+});
+
 app.get("/usuario/buscarnome/:nome", async (req, res) => {
   const { nome } = req.params;
-  const [query] = await conection.execute(
+  const [query] = await connection.execute(
     "select * from usuario where nome like ?",
     ["%" + nome + "%"]
   );
   if (query.length === 0)
-    return res
-      .status(400)
-      .json({ mensagem: "Nenhum usuário encontrado por este nome!" });
+    return res.status(400).json({ mensagem: "Nenhum usuario encontrado" });
   return res.status(200).json(query);
 });
 
-// retorna o usuário pelo email
 app.get("/usuario/buscaremail/:email", async (req, res) => {
   const { email } = req.params;
-  const [query] = await conection.execute(
+  const [query] = await connection.execute(
     "select * from usuario where email like ?",
     ["%" + email + "%"]
   );
   if (query.length === 0)
-    return res
-      .status(400)
-      .json({ mensagem: "Nenhum usuário encontrado por este email!" });
+    return res.status(400).json({ mensagem: "Nenhum usuario encontrado" });
   return res.status(200).json(query);
 });
 
-// insere um novo usuário no database
-app.post("/usuario", async (req, res) => {
-  const { nome, sobrenome, email, senha, cpf } = req.body;
-  const [query] = await conection.execute(
-    "insert into usuario (nome, sobrenome, email, senha, cpf) values (?, ?, ?, ?, ?)",
-    [nome, sobrenome, email, senha, cpf]
+app.get("/categoria/receita/buscarnome/:nome", async (req, res) => {
+  const { nome } = req.params;
+  const [query] = await connection.execute(
+    "select * from categoria_receita where nome like ?",
+    ["%" + nome + "%"]
   );
-  return res.json(query);
+  if (query.length === 0)
+    return res.status(400).json({ mensagem: "Nenhuma receita encontrada" });
+  return res.status(200).json(query);
 });
 
-// atualiza os dados do usuário no database
+app.get("/categoria/receita/buscartipo/:tipo", async (req, res) => {
+  const { tipo } = req.params;
+  const [query] = await connection.execute(
+    "select * from categoria_receita where tipo like ?",
+    ["%" + tipo + "%"]
+  );
+  if (query.length === 0)
+    return res.status(400).json({ mensagem: "Nenhuma receita encontrada" });
+  return res.status(200).json(query);
+});
+
+app.get("/categoria/despesa/buscarnome/:nome", async (req, res) => {
+  const { nome } = req.params;
+  const [query] = await connection.execute(
+    "select * from categoria_despesa where nome like ?",
+    ["%" + nome + "%"]
+  );
+  if (query.length === 0)
+    return res.status(400).json({ mensagem: "Nenhuma despesa encontrada" });
+  return res.status(200).json(query);
+});
+
+app.get("/categoria/despesa/buscartipo/:tipo", async (req, res) => {
+  const { tipo } = req.params;
+  const [query] = await connection.execute(
+    "select * from categoria_despesa where tipo like ?",
+    ["%" + tipo + "%"]
+  );
+  if (query.length === 0)
+    return res.status(400).json({ mensagem: "Nenhuma despesa encontrada" });
+  return res.status(200).json(query);
+});
+
+app.get("/despesas/buscardescricao/:descricao", async (req, res) => {
+  const { descricao } = req.params;
+  const [query] = await connection.execute(
+    "select * from despesas where descricao like ?",
+    ["%" + descricao + "%"]
+  );
+  if (query.length === 0)
+    return res.status(400).json({ mensagem: "Nenhuma despesa encontrada" });
+  return res.status(200).json(query);
+});
+
+app.get("/despesas/buscardata/:data", async (req, res) => {
+  const { data } = req.params;
+  const [query] = await connection.execute(
+    "select * from despesas where data like ?",
+    ["%" + data + "%"]
+  );
+  if (query.length === 0)
+    return res.status(400).json({ mensagem: "Nenhuma despesa encontrada" });
+  return res.status(200).json(query);
+});
+
+app.get("/despesas/buscarcategoria/:categoria", async (req, res) => {
+  const { categoria } = req.params;
+  const [query] = await connection.execute(
+    "select * from despesas where categoria_despesa_id like ?",
+    ["%" + categoria + "%"]
+  );
+  if (query.length === 0)
+    return res.status(400).json({ mensagem: "Nenhuma despesa encontrada" });
+  return res.status(200).json(query);
+});
+
+app.get("/receitas/buscardescricao/:descricao", async (req, res) => {
+  const { descricao } = req.params;
+  const [query] = await connection.execute(
+    "select * from receitas where descricao like ?",
+    ["%" + descricao + "%"]
+  );
+  if (query.length === 0)
+    return res.status(400).json({ mensagem: "Nenhuma receita encontrada" });
+  return res.status(200).json(query);
+});
+
+app.get("/receitas/buscardata/:data", async (req, res) => {
+  const { data } = req.params;
+  const [query] = await connection.execute(
+    "select * from receitas where data like ?",
+    ["%" + data + "%"]
+  );
+  if (query.length === 0)
+    return res.status(400).json({ mensagem: "Nenhuma receita encontrada" });
+  return res.status(200).json(query);
+});
+
+app.get("/receita/buscarcategoria/:categoria", async (req, res) => {
+  const { categoria } = req.params;
+  const [query] = await connection.execute(
+    "select * from receitas where categoria_receita_id like ?",
+    ["%" + categoria + "%"]
+  );
+  if (query.length === 0)
+    return res.status(400).json({ mensagem: "Nenhuma receita encontrada" });
+  return res.status(200).json(query);
+});
+
 app.put("/usuario/:id", async (req, res) => {
   const { id } = req.params;
-  const { nome, sobrenome, email, senha, cpf } = req.body;
-  const [query] = await conection.execute(
-    "update usuario set nome = ?, sobrenome = ?, email = ?, senha = ?, cpf = ? where id = ?",
-    [nome, sobrenome, email, senha, cpf, id]
+  const { nome, email, senha } = req.body;
+
+  const [query] = await connection.execute(
+    "update usuario set nome = ?, email = ?, senha = ? where id = ?",
+    [nome, email, senha, id]
   );
   return res.json(query);
 });
 
-//  deleta o usuário do database
+app.put("/categoria/receita/:id", async (req, res) => {
+  const { id } = req.params;
+  const { nome, tipo } = req.body;
+
+  const [query] = await connection.execute(
+    "update categoria_receita set nome = ?, tipo = ? where id = ?",
+    [nome, tipo, id]
+  );
+  return res.json(query);
+});
+
+app.put("/categoria/despesa/:id", async (req, res) => {
+  const { id } = req.params;
+  const { nome, tipo } = req.body;
+
+  const [query] = await connection.execute(
+    "update categoria_despesa set nome = ?, tipo = ? where id = ?",
+    [nome, tipo, id]
+  );
+  return res.json(query);
+});
+
+app.put("/despesas/:id", async (req, res) => {
+  const { id } = req.params;
+  const { descricao, valor, data, categoria_despesa_id } = req.body;
+
+  const [query] = await connection.execute(
+    "update despesas set descricao = ?, valor = ?, data = ?, categoria_despesa_id = ? where id = ?",
+    [descricao, valor, data, categoria_despesa_id, id]
+  );
+  return res.json(query);
+});
+
+app.put("/receitas/:id", async (req, res) => {
+  const { id } = req.params;
+  const { descricao, valor, data, categoria_receita_id } = req.body;
+
+  const [query] = await connection.execute(
+    "update receitas set descricao = ?, valor = ?, data = ?, categoria_receita_id = ? where id = ?",
+    [descricao, valor, data, categoria_receita_id, id]
+  );
+  return res.json(query);
+});
+
 app.delete("/usuario/:id", async (req, res) => {
   const { id } = req.params;
-  const [query] = await conection.execute("delete from usuario where id = ?", [
+  const [query] = await connection.execute("delete from usuario where id = ?", [
     id,
   ]);
   return res.json(query);
 });
 
-// categoriaReceita
-// seleciona todos os usuários do database
-const getAllCategoriaReceitas = async () => {
-  const [query] = await conection.execute("select * from categoria_receita");
-  return query;
-};
-
-// retorna a lista de usuários
-app.get("/categoriareceita", async (req, res) => {
-  const resultado = await getAllCategoriaReceitas();
-
-  if (resultado.length === 0) {
-    return res
-      .status(200)
-      .json({ mensagem: "Nenhuma categoria-receita encontrado no database!" });
-  }
-  return res.status(200).json(resultado);
-});
-
-// retorna o usuário pelo seu id
-app.get("/categoriareceita/:id", async (req, res) => {
+app.delete("/categoria/receita/:id", async (req, res) => {
   const { id } = req.params;
-  const [query] = await conection.execute(
-    "select * from categoria_receita where id = ?",
-    [id]
-  );
-  if (query.length === 0)
-    return res
-      .status(400)
-      .json({
-        mensagem:
-          "Nenhuma categoria-receita encontrado por este id no database!",
-      });
-  return res.status(200).json(query);
-});
-
-// retorna o usuário pelo nome
-app.get("/categoriareceita/buscarcategoria/:nome", async (req, res) => {
-  const { nome } = req.params;
-  const [query] = await conection.execute(
-    "select * from categoria_receita where nome like ?",
-    ["%" + nome + "%"]
-  );
-  if (query.length === 0)
-    return res
-      .status(400)
-      .json({
-        mensagem: "Nenhuma categoria-receita encontrado por este nome!",
-      });
-  return res.status(200).json(query);
-});
-
-// retorna o usuário pelo email
-app.get("/categoriareceita/buscarcategoria/:tipo", async (req, res) => {
-  const { tipo } = req.params;
-  const [query] = await conection.execute(
-    "select * from categoria_receita where tipo like ?",
-    ["%" + tipo + "%"]
-  );
-  if (query.length === 0)
-    return res
-      .status(400)
-      .json({
-        mensagem: "Nenhuma categoria-receita encontrado por este tipo!",
-      });
-  return res.status(200).json(query);
-});
-
-// insere um novo usuário no database
-app.post("/categoriareceita", async (req, res) => {
-  const { nome, tipo, usuario_id } = req.body;
-  const [query] = await conection.execute(
-    "insert into categoria_receita (nome, tipo, usuario_id) values (?, ?, ?)",
-    [nome, tipo, usuario_id]
-  );
-  return res.json(query);
-});
-
-// atualiza os dados do usuário no database
-app.put("/categoriareceita/:id", async (req, res) => {
-  const { id } = req.params;
-  const { nome, tipo } = req.body;
-  const [query] = await conection.execute(
-    "update categoria_receita set nome = ?, tipo = ? where id = ?",
-    [nome, tipo]
-  );
-  return res.json(query);
-});
-
-//  deleta o usuário do database
-app.delete("/categoriareceita/:id", async (req, res) => {
-  const { id } = req.params;
-  const [query] = await conection.execute(
+  const [query] = await connection.execute(
     "delete from categoria_receita where id = ?",
     [id]
   );
   return res.json(query);
 });
 
-// categoriaDispesas
-// seleciona todos os usuários do database
-const getAllCategoriaDispesa = async () => {
-  const [query] = await conection.execute("select * from categoria_despesa");
-  return query;
-};
-
-// retorna a lista de usuários
-app.get("/categoriadispesa", async (req, res) => {
-  const resultado = await getAllCategoriaDispesa();
-
-  if (resultado.length === 0) {
-    return res
-      .status(200)
-      .json({ mensagem: "Nenhuma categoria-dispesa encontrado no database!" });
-  }
-  return res.status(200).json(resultado);
-});
-
-// retorna o usuário pelo seu id
-app.get("/categoriareceita/:id", async (req, res) => {
+app.delete("/categoria/despesa/:id", async (req, res) => {
   const { id } = req.params;
-  const [query] = await conection.execute(
-    "select * from categoria_receita where id = ?",
+  const [query] = await connection.execute(
+    "delete from categoria_despesa where id = ?",
     [id]
   );
-  if (query.length === 0)
-    return res
-      .status(400)
-      .json({
-        mensagem:
-          "Nenhuma categoria-receita encontrado por este id no database!",
-      });
-  return res.status(200).json(query);
+  return res.json(query);
 });
 
-// retorna o usuário pelo nome
-app.get("/categoriareceita/buscarcategoria/:nome", async (req, res) => {
-  const { nome } = req.params;
-  const [query] = await conection.execute(
-    "select * from categoria_receita where nome like ?",
-    ["%" + nome + "%"]
-  );
-  if (query.length === 0)
-    return res
-      .status(400)
-      .json({
-        mensagem: "Nenhuma categoria-receita encontrado por este nome!",
-      });
-  return res.status(200).json(query);
-});
-
-// retorna o usuário pelo email
-app.get("/categoriareceita/buscarcategoria/:tipo", async (req, res) => {
-  const { tipo } = req.params;
-  const [query] = await conection.execute(
-    "select * from categoria_receita where tipo like ?",
-    ["%" + tipo + "%"]
-  );
-  if (query.length === 0)
-    return res
-      .status(400)
-      .json({
-        mensagem: "Nenhuma categoria-receita encontrado por este tipo!",
-      });
-  return res.status(200).json(query);
-});
-
-// insere um novo usuário no database
-app.post("/categoriareceita", async (req, res) => {
-  const { nome, tipo, usuario_id } = req.body;
-  const [query] = await conection.execute(
-    "insert into categoria_receita (nome, tipo, usuario_id) values (?, ?, ?)",
-    [nome, tipo, usuario_id]
+app.delete("/despesas/:id", async (req, res) => {
+  const { id } = req.params;
+  const [query] = await connection.execute(
+    "delete from despesas where id = ?",
+    [id]
   );
   return res.json(query);
 });
 
-// atualiza os dados do usuário no database
-app.put("/categoriareceita/:id", async (req, res) => {
+app.delete("/receitas/:id", async (req, res) => {
   const { id } = req.params;
-  const { nome, tipo } = req.body;
-  const [query] = await conection.execute(
-    "update categoria_receita set nome = ?, tipo = ? where id = ?",
-    [nome, tipo]
-  );
-  return res.json(query);
-});
-
-//  deleta o usuário do database
-app.delete("/categoriareceita/:id", async (req, res) => {
-  const { id } = req.params;
-  const [query] = await conection.execute(
-    "delete from categoria_receita where id = ?",
+  const [query] = await connection.execute(
+    "delete from receitas where id = ?",
     [id]
   );
   return res.json(query);
